03872356a471e82ac215cfdd5656c1758cce29a2
Jeremy Allard <jeremy@allard.io>
2020-02-21 22:34:48 -0500

    Improve Onboarding layout and content

--- src/Onboarding.tsx
+++ src/Onboarding.tsx
@@ -1,48 +1,112 @@
 import React from "react";
-import { FaMusic, FaReact, FaSmile } from "react-icons/fa";
+import { FaMusic, FaReact, FaSearch } from "react-icons/fa";
+import { useMedia } from "react-use";
 import styled from "styled-components";
+import media from "styled-media-query";
+import { color, ColorStyleProps, fontSize, FontSizeProps } from "styled-system";
 import Button from "ui/Button";
 
-const Heading = styled.h1`
+import theme from "./theme";
+
+const Heading = styled.h1<FontSizeProps & ColorStyleProps>`
+  display: inline-flex;
+  flex-direction: row;
+  align-items: center;
   font-family: Aileron;
-  margin: 0;
-  font-size: 4rem;
+  color: ${({ theme }) => theme.colors.secondary[9]};
+  ${fontSize}
+  ${color}
 `;
 
-const List = styled.ul`
-  font-family: Aileron;
-  list-style-type: none;
-  margin: 0;
-  padding: 0;
+const IconWrapper = styled.div`
+  margin-right: 0.5rem;
+  display: flex;
 `;
 
-const ListElement = styled.li`
-  color: ${props => props.theme.colors.secondary[9]};
-  margin: 5px 0 0 0;
-  padding: 0;
+const Text = styled.p`
+  font-family: Aileron;
+  color: ${({ theme }) => theme.colors.secondary[9]};
+`;
+
+const Section = styled.section``;
+
+const Footer = styled.footer`
+  padding: 1rem;
+  ${Button} {
+    display: block;
+    margin-left: auto;
+    width: 100%;
+    ${media.greaterThan("medium")`
+      width: auto;
+    `}
+  }
+`;
+
+const Header = styled.header`
+  display: flex;
+  flex-direction: column;
+  padding: 1rem;
+
+  ${Heading} {
+    margin: 0;
+  }
 `;
 
 type OnboardingProps = { onStarted?: () => void; className?: string };
 const Onboarding: React.FC<OnboardingProps> = ({ onStarted, className }) => {
+  const isDesktop = useMedia("(min-width: 768px)", false);
+
   return (
     <div className={className}>
-      <Heading>Tinged.</Heading>
+      <Header>
+        <Heading fontSize={8}>Tinged</Heading>
+        <Heading fontSize={4}>A modern and powerful media player</Heading>
+      </Header>
 
-      <List>
-        <ListElement>
-          <FaMusic /> Listen to your favorite songs
-        </ListElement>
+      <Section>
+        <Heading fontSize={3}>
+          <IconWrapper>
+            <FaMusic />
+          </IconWrapper>
+          Listen to your favorite hits
+        </Heading>
 
-        <ListElement>
-          <FaSmile /> Using a clean and neat interface
-        </ListElement>
+        <Text>
+          Enjoy your favorite songs using a beautiful and clutter-free
+          interface.
+        </Text>
+      </Section>
 
-        <ListElement>
-          <FaReact /> Made with modern web technologies
-        </ListElement>
-      </List>
+      <Section>
+        <Heading fontSize={4}>
+          <IconWrapper>
+            <FaSearch />
+          </IconWrapper>
+          Find your favorite hits
+        </Heading>
+        <Text>
+          Search through your Tinged library to find the hits that you want
+          quickly.
+        </Text>
+      </Section>
 
-      <Button onClick={onStarted}>Start</Button>
+      <Section>
+        <Heading fontSize={4}>
+          <IconWrapper>
+            <FaReact />
+          </IconWrapper>
+           Made with modern technologies
+        </Heading>
+
+        <Text>
+          Tinged runs on Electron and was implemented using TypeScript and
+          React.
+        </Text>
+      </Section>
+
+      <Footer>
+        <Button onClick={onStarted}>Start</Button>
+      </Footer>
     </div>
   );
 };
@@ -51,22 +115,33 @@ const StyledOnboarding = styled(Onboarding)`
   display: flex;
   flex-direction: column;
   height: 100%;
+  box-sizing: border-box;
 
-  ${Heading} {
+  ${Header} {
+    padding: 2rem;
+  }
+
+  ${Section} {
     flex: 1;
-    margin-left: 2rem;
-    margin-top: 2rem;
-  }
+    padding: 2rem;
+    :nth-of-type(2) {
+      border-top: ${({ theme }) => `1px solid ${theme.colors.secondary[1]}`};
+      border-bottom: ${({ theme }) => `1px solid ${theme.colors.secondary[1]}`};
 
-  ${List} {
-    flex: 3;
-    margin-left: 2rem;
-    margin-top: 3rem;
-    margin-bottom: 3rem;
-  }
+      background-color: ${({ theme }) => theme.colors.secondary[0]};
 
-  ${Button} {
-    margin: auto 2rem 2rem 2rem;
+      ${Text} {
+        color: ${({ theme }) => theme.colors.secondary[8]};
+        font-weight: 500;
+      }
+      ${Heading} {
+        color: ${({ theme }) => theme.colors.secondary[8]};
+      }
+
+      ${media.greaterThan("medium")`
+      text-align: right;
+    `}
+    }
   }
 `;
 
